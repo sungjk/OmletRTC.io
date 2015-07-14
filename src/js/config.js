f90b9deaafa4d5c491fe311cfcdd53908a09c8c5
sungjungkim <ajax0615@gmail.com>
2015-07-14 09:00:31 -0700

    app configuration

---
+++ src/js/config.js
@@ -0,0 +1,17 @@
+// Server Configuration File
+
+// Web-Server
+var ws = {};
+module.exports.ws = ws;
+ws.port = 8080;
+ws.securePort = 443;
+ws.secured = false; //HTTPS support
+
+//Database
+var db = {};
+module.exports.db = db;
+db.present = false;  // DB presence
+db.host = 'localhost';
+db.user     = 'user' ;
+db.password = 'password' ;
+db.database = 'webrtcdb' ;
