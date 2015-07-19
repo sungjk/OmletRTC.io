3d1fc79c362f7ab7ac277e5a7839a6120778dd72
sungjungkim <ajax0615@gmail.com>
2015-07-18 22:13:38 -0700

    add dynamicConstraints

--- src/js/main.js
+++ src/js/main.js
@@ -537,7 +537,7 @@ function getLocalMedia(){
   //   audio: false, 
   //   video: true
   // }, localStreaming, logError);
-  navigator.getUserMedia(dynamicConstraints, localStreaming, logError);
+  navigator.getUserMedia(srcConstraints, localStreaming, logError);
 }
 
 
@@ -546,7 +546,7 @@ function getRemoteMedia() {
   //   audio: false,
   //   video: true
   // }, remoteStreaming, logError);
-  navigator.getUserMedia(dynamicConstraints, remoteStreaming, logError);
+  navigator.getUserMedia(srcConstraints, remoteStreaming, logError);
 }
 
 
