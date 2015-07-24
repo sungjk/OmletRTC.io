fc7654a2a8ab66ccefb8beabf3a2d531250a1e7e
sungjungkim <ajax0615@gmail.com>
2015-07-24 14:39:43 -0700

    attachMediaStream가 포함된 adapter

--- src/js/adapter.js
+++ src/js/adapter.js
@@ -1,169 +1,326 @@
-//////////////////////////////////////////////////////////////////
-//
-//                Variables          
-//
-/////////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////
+// //
+// //                Variables          
+// //
+// /////////////////////////////////////////////////////////////////
+
+// var RTCPeerConnection = null;
+// var getUserMedia = null;
+// var connectStreamToSrc = null;
+// var onMessage = null ;
+// var detectedBrowser = null;
+
+// var width = screen.availWidth / 2;
+// var height = screen.availHeight / 2;
+
+// var attachMediaStream = null;
+
+
+// // default contraints object
+// var constraints = { 
+//     video: true, 
+//     audio: false 
+// };
+
+// // Constraints object for mobile size
+// var dynamicConstraints = {
+//     audio: false,
+//     video: {
+//         mandatory: {
+//             maxHeight: screen.availWidth / 2,
+//             maxWidth: screen.availWidth / 2
+//         }
+//     }
+// };
+
+// // Original constraints object for web app video
+// var srcConstraints = {
+//   audio: false,
+//   video: {
+//     mandatory: {
+//       //minFrameRate: 30,
+//       maxHeight: 240,
+//       maxWidth: 320
+//     }
+//   }
+// };
+
+// // Constraints object for low resolution video
+// var qvgaConstraints = { 
+//   audio: false,
+//   video: {
+//     mandatory: {
+//       maxWidth: 320,
+//       maxHeight: 240
+//     } 
+//   }
+// };
+
+// // Constraints object for standard resolution video
+// var vgaConstraints = { 
+//   audio: false,
+//   video: {
+//     mandatory: {
+//       maxWidth: 640,
+//       maxHeight: 480
+//     } 
+//   }
+// };
+
+// // Constraints object for high resolution video
+// var hdConstraints = { 
+//   audio: false,
+//   video: {
+//     mandatory: {
+//       maxWidth: 1280,
+//       maxHeight: 960
+//     } 
+//   }
+// };
+
+
+// if (navigator.getUserMedia) {
+//     // WebRTC standard
+//     RTCPeerConnection = RTCPeerConnection;
+//     getUserMedia = navigator.getUserMedia.bind(navigator);
+// } else if (navigator.mozGetUserMedia) {
+//     // early Firefox
+//     detectedBrowser = "Firefox" ;
+//     RTCPeerConnection = mozRTCPeerConnection;
+//     RTCSessionDescription = mozRTCSessionDescription;
+//     RTCIceCandidate = mozRTCIceCandidate;
+//     getUserMedia = navigator.mozGetUserMedia.bind(navigator);
+//     onMessage = function (event) {
+//         var t_msg = time() ;
+//         var blob = event.data; // Firefox allows sending blobs directly
+//         if( typeof blob === "object" )
+//         {
+//             var reader = new window.FileReader();
+//             reader.readAsDataURL(blob);
+//             reader.onload = function (event) {
+//                 var fileDataURL = event.target.result; // it is Data URL...can be saved to disk
+//                 saveToDisk(fileDataURL, 'fake fileName');
+//             };
+//         }
+//         else
+//         {
+//             appendDIV(event.data);
+//             log("Text message received at " + t_msg.toString() ) ;
+//         }
+//     };
+// }
+// else if (navigator.webkitGetUserMedia) {
+//     detectedBrowser = "Chrome" ;
+//     RTCPeerConnection = webkitRTCPeerConnection;
+//     getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
+// } else {
+//     alert("WebRTC is not supported.");
+// }
+
+// var videoSourceId = null ;
+// var audioSourceId = null ;
+
+// function gotSources(sourceInfos) {
+//     for (var i = 0; i != sourceInfos.length; ++i) {
+//         var sourceInfo = sourceInfos[i];
+//         if (sourceInfo.kind === 'audio') {
+//             //log('Audio ' + sourceInfo.label ) ;
+//             if( audioSourceId == null )
+//                 audioSourceId = sourceInfo.id ;
+//         } else if (sourceInfo.kind === 'video') {
+//             //log('Video source found: ' + sourceInfo.label ) ;
+//             //if( sourceInfo.label.indexOf("facing back") != -1 )
+//             //{
+//                 //videoSourceId  = sourceInfo.id ;
+//                 //log("Found " + videoSourceId ) ;
+//             //}
+//         } else {
+//             log('Some other kind of source: ', sourceInfo);
+//         }
+//     }
+// }
+// if (detectedBrowser == "Chrome") {
+//     MediaStreamTrack.getSources(gotSources);
+// }
+// var server = {
+//     "iceServers": [
+//     //{url:"stun:stun.services.mozilla.com"}//,
+//     //{url:"stun:203.246.112.144:3310"},
+//     {url:"stun:stun.l.google.com:19302"}]
+// };
+
+
+// // Look after different browser vendors' ways of calling the getUserMedia() API method:
+// navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+
+
+// attachMediaStream = function(element, stream) {
+//   if (window.URL) element.src = window.URL.createObjectURL(stream);
+//   else            element.src = stream;
+  
+//   element.autoplay = true;
+//   element.play();  
+// };
+
+
+
+
 
 var RTCPeerConnection = null;
 var getUserMedia = null;
-var connectStreamToSrc = null;
-var onMessage = null ;
-var detectedBrowser = null;
-
-var width = screen.availWidth / 2;
-var height = screen.availHeight / 2;
-
 var attachMediaStream = null;
+var reattachMediaStream = null;
+var webrtcDetectedBrowser = null;
+var webrtcDetectedVersion = null;
 
-
-// default contraints object
-var constraints = { 
-    video: true, 
-    audio: false 
-};
-
-// Constraints object for mobile size
-var dynamicConstraints = {
-    audio: false,
-    video: {
-        mandatory: {
-            maxHeight: screen.availWidth / 2,
-            maxWidth: screen.availWidth / 2
-        }
-    }
-};
-
-// Original constraints object for web app video
-var srcConstraints = {
-  audio: false,
-  video: {
-    mandatory: {
-      //minFrameRate: 30,
-      maxHeight: 240,
-      maxWidth: 320
-    }
+function trace(text) {
+  // This function is used for logging.
+  if (text[text.length - 1] == '\n') {
+    text = text.substring(0, text.length - 1);
   }
-};
-
-// Constraints object for low resolution video
-var qvgaConstraints = { 
-  audio: false,
-  video: {
-    mandatory: {
-      maxWidth: 320,
-      maxHeight: 240
-    } 
-  }
-};
-
-// Constraints object for standard resolution video
-var vgaConstraints = { 
-  audio: false,
-  video: {
-    mandatory: {
-      maxWidth: 640,
-      maxHeight: 480
-    } 
-  }
-};
-
-// Constraints object for high resolution video
-var hdConstraints = { 
-  audio: false,
-  video: {
-    mandatory: {
-      maxWidth: 1280,
-      maxHeight: 960
-    } 
-  }
-};
-
-
-if (navigator.getUserMedia) {
-    // WebRTC standard
-    RTCPeerConnection = RTCPeerConnection;
-    getUserMedia = navigator.getUserMedia.bind(navigator);
-} else if (navigator.mozGetUserMedia) {
-    // early Firefox
-    detectedBrowser = "Firefox" ;
-    RTCPeerConnection = mozRTCPeerConnection;
-    RTCSessionDescription = mozRTCSessionDescription;
-    RTCIceCandidate = mozRTCIceCandidate;
-    getUserMedia = navigator.mozGetUserMedia.bind(navigator);
-    /*onMessage = function (event) {
-        var t_msg = time() ;
-        var blob = event.data; // Firefox allows sending blobs directly
-        if( typeof blob === "object" )
-        {
-            var reader = new window.FileReader();
-            reader.readAsDataURL(blob);
-            reader.onload = function (event) {
-                var fileDataURL = event.target.result; // it is Data URL...can be saved to disk
-                saveToDisk(fileDataURL, 'fake fileName');
-            };
-        }
-        else
-        {
-            appendDIV(event.data);
-            log("Text message received at " + t_msg.toString() ) ;
-        }
-    };*/
+  console.log((performance.now() / 1000).toFixed(3) + ": " + text);
 }
-else if (navigator.webkitGetUserMedia) {
-    detectedBrowser = "Chrome" ;
-    RTCPeerConnection = webkitRTCPeerConnection;
-    getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
+
+if (navigator.mozGetUserMedia) {
+  console.log("This appears to be Firefox");
+
+  webrtcDetectedBrowser = "firefox";
+
+  webrtcDetectedVersion =
+                  parseInt(navigator.userAgent.match(/Firefox\/([0-9]+)\./)[1]);
+
+  // The RTCPeerConnection object.
+  RTCPeerConnection = mozRTCPeerConnection;
+
+  // The RTCSessionDescription object.
+  RTCSessionDescription = mozRTCSessionDescription;
+
+  // The RTCIceCandidate object.
+  RTCIceCandidate = mozRTCIceCandidate;
+
+  // Get UserMedia (only difference is the prefix).
+  // Code from Adam Barth.
+  getUserMedia = navigator.mozGetUserMedia.bind(navigator);
+
+  // Creates iceServer from the url for FF.
+  createIceServer = function(url, username, password) {
+    var iceServer = null;
+    var url_parts = url.split(':');
+    if (url_parts[0].indexOf('stun') === 0) {
+      // Create iceServer with stun url.
+      iceServer = { 'url': url };
+    } else if (url_parts[0].indexOf('turn') === 0 &&
+               (url.indexOf('transport=udp') !== -1 ||
+                url.indexOf('?transport') === -1)) {
+      // Create iceServer with turn url.
+      // Ignore the transport parameter from TURN url.
+      var turn_url_parts = url.split("?");
+      iceServer = { 'url': turn_url_parts[0],
+                    'credential': password,
+                    'username': username };
+    }
+    return iceServer;
+  };
+
+  // Attach a media stream to an element.
+  attachMediaStream = function(element, stream) {
+    console.log("Attaching media stream");
+    element.mozSrcObject = stream;
+    element.play();
+  };
+
+  reattachMediaStream = function(to, from) {
+    console.log("Reattaching media stream");
+    to.mozSrcObject = from.mozSrcObject;
+    to.play();
+  };
+
+  // Fake get{Video,Audio}Tracks
+  MediaStream.prototype.getVideoTracks = function() {
+    return [];
+  };
+
+  MediaStream.prototype.getAudioTracks = function() {
+    return [];
+  };
+} else if (navigator.webkitGetUserMedia) {
+  console.log("This appears to be Chrome");
+
+  webrtcDetectedBrowser = "chrome";
+  webrtcDetectedVersion =
+             parseInt(navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./)[2]);
+
+  // Creates iceServer from the url for Chrome.
+  createIceServer = function(url, username, password) {
+    var iceServer = null;
+    var url_parts = url.split(':');
+    if (url_parts[0].indexOf('stun') === 0) {
+      // Create iceServer with stun url.
+      iceServer = { 'url': url };
+    } else if (url_parts[0].indexOf('turn') === 0) {
+      if (webrtcDetectedVersion < 28) {
+        // For pre-M28 chrome versions use old TURN format.
+        var url_turn_parts = url.split("turn:");
+        iceServer = { 'url': 'turn:' + username + '@' + url_turn_parts[1],
+                      'credential': password };
+      } else {
+        // For Chrome M28 & above use new TURN format.
+        iceServer = { 'url': url,
+                      'credential': password,
+                      'username': username };
+      }
+    }
+    return iceServer;
+  };
+
+  // The RTCPeerConnection object.
+  RTCPeerConnection = webkitRTCPeerConnection;
+
+  // Get UserMedia (only difference is the prefix).
+  // Code from Adam Barth.
+  getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
+
+  // Attach a media stream to an element.
+  attachMediaStream = function(element, stream) {
+    if (typeof element.srcObject !== 'undefined') {
+      element.srcObject = stream;
+    } else if (typeof element.mozSrcObject !== 'undefined') {
+      element.mozSrcObject = stream;
+    } else if (typeof element.src !== 'undefined') {
+      element.src = URL.createObjectURL(stream);
+    } else {
+      console.log('Error attaching stream to element.');
+    }
+  };
+
+  reattachMediaStream = function(to, from) {
+    to.src = from.src;
+  };
+
+  // The representation of tracks in a stream is changed in M26.
+  // Unify them for earlier Chrome versions in the coexisting period.
+  if (!webkitMediaStream.prototype.getVideoTracks) {
+    webkitMediaStream.prototype.getVideoTracks = function() {
+      return this.videoTracks;
+    };
+    webkitMediaStream.prototype.getAudioTracks = function() {
+      return this.audioTracks;
+    };
+  }
+
+  // New syntax of getXXXStreams method in M26.
+  if (!webkitRTCPeerConnection.prototype.getLocalStreams) {
+    webkitRTCPeerConnection.prototype.getLocalStreams = function() {
+      return this.localStreams;
+    };
+    webkitRTCPeerConnection.prototype.getRemoteStreams = function() {
+      return this.remoteStreams;
+    };
+  }
 } else {
-    alert("WebRTC is not supported.");
+  console.log("Browser does not appear to be WebRTC-capable");
 }
-
-var videoSourceId = null ;
-var audioSourceId = null ;
-
-function gotSources(sourceInfos) {
-    for (var i = 0; i != sourceInfos.length; ++i) {
-        var sourceInfo = sourceInfos[i];
-        if (sourceInfo.kind === 'audio') {
-            //log('Audio ' + sourceInfo.label ) ;
-            if( audioSourceId == null )
-                audioSourceId = sourceInfo.id ;
-        } else if (sourceInfo.kind === 'video') {
-            //log('Video source found: ' + sourceInfo.label ) ;
-            //if( sourceInfo.label.indexOf("facing back") != -1 )
-            //{
-                //videoSourceId  = sourceInfo.id ;
-                //log("Found " + videoSourceId ) ;
-            //}
-        } else {
-            log('Some other kind of source: ', sourceInfo);
-        }
-    }
-}
-if (detectedBrowser == "Chrome") {
-    MediaStreamTrack.getSources(gotSources);
-}
-var server = {
-    "iceServers": [
-    //{url:"stun:stun.services.mozilla.com"}//,
-    //{url:"stun:203.246.112.144:3310"},
-    {url:"stun:stun.l.google.com:19302"}]
-};
-
-
-// Look after different browser vendors' ways of calling the getUserMedia() API method:
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-
-
-attachMediaStream = function(element, stream) {
-  if (window.URL) element.src = window.URL.createObjectURL(stream);
-  else            element.src = stream;
-  
-  element.autoplay = true;
-  element.play();  
-};
-
-
 
 
 
