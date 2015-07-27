df912da95da47caf584f1a0e4221fe148a17c142
sungjungkim <ajax0615@gmail.com>
2015-07-27 12:26:55 -0700

    최초 수정후 디버깅#1 - addMessage 조건문 수정

--- src/js/main.js
+++ src/js/main.js
@@ -757,7 +757,7 @@ function addMessage(old, parameters) {
 
   // if (parameters.message === 'usermedia')
   //   continue;
-  else if (parameters.message === 'create' || parameters.message === 'join') {
+  if (parameters.message === 'create' || parameters.message === 'join') {
     old.numOfUser = old.numOfUser + 1;
   }
   else if (parameters.message === 'candidate') {
