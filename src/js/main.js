fcd2c0be2dad79fccb1c6ac71633a2d7dff450ed
sungjungkim <ajax0615@gmail.com>
2015-07-27 23:22:34 -0700

    최초 수정후 디버깅#2 - prompt 추가

--- src/js/main.js
+++ src/js/main.js
@@ -870,6 +870,8 @@ function addUser(doc) {
 // Clean-up function: collect garbage before unloading browser's window
 window.onbeforeunload = clearDocument;
 
+var room = prompt('Enter room name:');
+
 function get(id){
   return document.getElementById(id);
 }
