c2720b71581d19c686e3932ce0beca47a4084141
sungjungkim <ajax0615@gmail.com>
2015-08-14 00:29:35 -0700

    #omletrtc express server

--- src/proto.server.js
+++ src/proto.server.js
@@ -7,7 +7,7 @@ var server = http.createServer(app).listen(3310, function() {
   console.log('[+] Set [http] protocol and server running at port #3310');
 });
 
-var webRTC = require('/webrtcjs/omletrtc').listen(server);
+var webRTC = require('./webrtcjs/omletrtc').listen(server);
 
 // Handle resource request by server
 app.get('/', function(req, res) {
