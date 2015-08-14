c2c7eb268d94bf52ed9da79c0e6185946a0a8fd6
sungjungkim <ajax0615@gmail.com>
2015-08-14 00:15:51 -0700

    #omletrtc express server

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ var config = require('./js/config');
 var http = require('http') ;
 var port = config.ws.port;
 
-var server = http.createServer(app).listen(port, function() {
+var server = http.createServer(app).listen(3310, function() {
   console.log('[+] Set [http] protocol and server running at port #' + port);
 });
 
@@ -32,5 +32,33 @@ app.get('/webrtcjs/omletrtc.js', function(req, res) {
 app.get('/js/omlet.js', function(req, res) {
   res.sendfile(__dirname + '/js/omlet.js');
 });
+
+/*
+webRTC.omletrtc.on('chat_msg', function(data, socket) {
+  var roomList = webRTC.omletrtc.rooms[data.room] || [];
+
+  for (var i = 0; i < roomList.length; i++) {
+    var socketId = roomList[i];
+
+    if (socketId !== socket.id) {
+      var soc = webRTC.omletrtc.getSocket(socketId);
+
+      if (soc) {
+        soc.send(JSON.stringify({
+          "eventName": "receive_chat_msg",
+          "data": {
+            "messages": data.messages,
+            "color": data.color
+          }
+        }), function(error) {
+          if (error) {
+            console.log(error);
+          }
+        });
+      }
+    }
+  }
+});
+*/
 
 app.use(express.static('.'));
