ae603255424520751c380b1d7cfcd196e758be4c
csg <cs09gi@gmail.com>
2015-08-18 00:46:59 -0700

    removed comments

--- src/webrtcjs/main.js
+++ src/webrtcjs/main.js
@@ -2,7 +2,6 @@ var documentApi;
 var myDocId;
 var chatDoc;
 
-//var videos = [];
 var PeerConnection = window.PeerConnection || window.webkitPeerConnection00 || window.webkitRTCPeerConnection || window.mozRTCPeerConnection || window.RTCPeerConnection;
 
 function log(message){
@@ -10,95 +9,6 @@ function log(message){
   logArea.value += message + '\n';
   logArea.scrollTop = logArea.scrollHeight;
 }
-
-/* unuse function
-function getNumPerRow() {
-  var len = videos.length;
-  var biggest;
-
-  // Ensure length is even for better division.
-  if(len % 2 === 1) {
-    len++;
-  }
-
-  biggest = Math.ceil(Math.sqrt(len));
-  while(len % biggest !== 0) {
-    biggest++;
-  }
-  return biggest;
-}
-*/
-
-/* unuse functions
-function subdivideVideos() {
-  var perRow = getNumPerRow();
-  var numInRow = 0;
-  for(var i = 0, len = videos.length; i < len; i++) {
-    var video = videos[i];
-    setWH(video, i);
-    numInRow = (numInRow + 1) % perRow;
-  }
-}
-
-function setWH(video, i) {
-  var perRow = getNumPerRow();
-  var perColumn = Math.ceil(videos.length / perRow);
-  var width = Math.floor((window.innerWidth) / perRow);
-  var height = Math.floor((window.innerHeight - 190) / perColumn);
-  video.width = width;
-  video.height = height;
-  video.style.position = "absolute";
-  video.style.left = (i % perRow) * width + "px";
-  video.style.top = Math.floor(i / perRow) * height + "px";
-}
-*/
-
-/* do not need this because there are only two peers
-function cloneVideo(domId, socketId) {
-  var video = document.getElementById(domId);
-  var clone = video.cloneNode(false);
-  clone.id = "remote" + socketId;
-  document.getElementById('videos').appendChild(clone);
-  videos.push(clone);
-  return clone;
-}
-
-
-function removeVideo(socketId) {
-  var video = document.getElementById('remote' + socketId);
-  if(video) {
-    videos.splice(videos.indexOf(video), 1); // find the index and remove it from the array
-    video.parentNode.removeChild(video); // remove from its parent tag
-  }
-}
-*/
-
-/* duplicated with "function create()"
-function createDocument() {
-  // do not need to let you click, just run the code 
-
-  //var button = document.getElementById("createDocument");
-  //button.addEventListener('click', function(event) {
-    if(!Omlet.isInstalled()) {
-      log("[-] Omlet is not installed.");
-    }
-    else {
-      log("[+] Omlet is installed.");
-      log("[+] DocumentAPI Obj:" + JSON.stringify(documentApi));
-
-      documentApi.create(function(d) {
-        myDocId = d.Document;
-        location.hash = "#/docId/" + myDocId;
-
-        documentApi.update(myDocId, Initialize, initConnectionInfo(), function() {
-          // update successCallback
-          documentApi.get(myDocId, DocumentCreated, errorCallback);
-        }, errorCallback);
-      }, errorCallback);
-    }
-  //});
-}
-*/
 
 function init() {
   if(PeerConnection) {
@@ -120,29 +30,14 @@ function init() {
 
   rtc.on('add remote stream', function(stream, socketId) {
     log("ADDING REMOTE STREAM...");
-    /* do not need to clone video due to it is only two peers
-    var clone = cloneVideo('you', socketId);
-    document.getElementById(clone.id).setAttribute("class", "");
-    rtc.attachStream(stream, clone.id);
-    */
 
     rtc.attachStream(stream, 'remoteVideo');
-
-    //subdivideVideos(); // don't need to calculate its width and height again
   });
   rtc.on('disconnect stream', function(data) {
     log('remove ' + data);
-    //removeVideo(data);
   });
-  // createDocument(); // duplicated with "function create()"
   create();
 }
-
-/* useless
-window.onresize = function(event) {
-  subdivideVideos();
-};
-*/
 
 /*
 * Omlet Framework
@@ -159,7 +54,6 @@ function initDocumentAPI() {
 
 function DocumentCreated(doc) {
   var callbackurl = "http://203.246.112.144:3310/video-calling-interface.html#/docId/" + myDocId;
-  //callbackurl = "http://203.246.112.144:3310/proto.index.html#/docId/" + myDocId; // duplicated
 
   if(Omlet.isInstalled()) {
     var rdl = Omlet.createRDL({
@@ -175,11 +69,7 @@ function DocumentCreated(doc) {
     Omlet.exit(rdl);
   }
 }
-/*
-function ReceiveDoc(doc) {
-  chatDoc = doc;
-}
-*/
+
 function _loadDocument() {
   if (hasDocument()) {
     myDocId = getDocumentReference();
@@ -202,10 +92,6 @@ function _loadDocument() {
 }
 
 function initConnectionInfo() {
-  //var chatId = 100;
-  //var identity = Omlet.getIdentity();
-  //var numOfUser = 0;
-
   // Connection info
   var info = {
     'chatId' : 100, /* random */
@@ -291,17 +177,8 @@ function addUser(doc) {
 }
 
 var createButton = get("createButton");
-//var clearButton = get("clearButton");
-//var getDocButton = get("getDocButton");
-//var joinDataButton = get("joinDataButton");
 
 createButton.onclick = create;
-//clearButton.onclick = clearDocument;
-//getDocButton.onclick = getDocument;
-//joinDataButton.onclick = joinData;
-
-//window.onbeforeunload = clearDocument;
-
 
 function get(id){
   return document.getElementById(id);
@@ -329,36 +206,6 @@ function create() {
   }
 }
 
-/*
-function clearDocument() {
-  if(!Omlet.isInstalled()) {
-    log("[-] Omlet is not installed.");
-  }
-  else {
-    log("[+] Clearing Document.");
-    stop();
-
-    documentApi.update(myDocId, addMessage, param_clear, function() {
-      documentApi.get(myDocId, DocumentCleared, function (error) {
-        log("[-] clearDocument-update-get: " + error);
-      });
-    }, function (error) {
-      log("[-] clearDocument-update: " + error);
-    });
-  }
-}
-*/
-/*
-function getDocument() {
-  if(!Omlet.isInstalled()) {
-    log("[-] Omlet is not installed.");
-  }
-  else {
-    documentApi.get(myDocId, ReceiveDoc, errorCallback);
-    log("[+] Getting Document. DocId: " + myDocId);
-  }
-}
-/*/
 Omlet.ready(function() {
   log("[+] Omlet is Ready.");
 
