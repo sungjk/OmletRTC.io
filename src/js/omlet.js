759b03ef13fb404a8cde4dc63c389670144ddeec
Seongjung Kim <ajax0615@gmail.com>
2015-08-17 18:46:10 -0700

    Revert "update UI"

--- src/js/omlet.js
+++ src/js/omlet.js
@@ -1,58 +1,61 @@
 /**
+ * Created by Saji on 4/7/2015.
+ */
+/**
  * Javascript inheritance
  * Source: http://joshgertzen.com/object-oriented-super-class-method-calling-with-javascript/
  */
 //Defines the top level Class
 function Class() { }
-    Class.prototype.construct = function() {};
-    Class.extend = function(def) {
-      var classDef = function() {
+Class.prototype.construct = function() {};
+Class.extend = function(def) {
+    var classDef = function() {
         if (arguments[0] !== Class) { this.construct.apply(this, arguments); }
-      };
- 
-      var proto = new this(Class);
-      var superClass = this.prototype;
- 
-      for (var n in def) {
-        var item = def[n];                      
+    };
+
+    var proto = new this(Class);
+    var superClass = this.prototype;
+
+    for (var n in def) {
+        var item = def[n];
         if (item instanceof Function) item.$ = superClass;
         proto[n] = item;
-      }
- 
-      classDef.prototype = proto;
- 
-      //Give this new class the same static extend method    
-  classDef.extend = this.extend;      
-  return classDef;
+    }
+
+    classDef.prototype = proto;
+
+    //Give this new class the same static extend method
+    classDef.extend = this.extend;
+    return classDef;
 };
 
 var Omlet = {
-  scope: false,
-  readyHandlers: [],
-  document: false, // holds documentApi
-  blob: false,
+    scope: false,
+    readyHandlers: [],
+    document: false, // holds documentApi
+    blob: false,
 };
 
 Omlet.createRDL = function(rdl) {
-  var obj = { "type" : "app", "data" : JSON.parse(JSON.stringify(rdl)) };
-  return obj;
+    var obj = { "type" : "app", "data" : JSON.parse(JSON.stringify(rdl)) };
+    return obj;
 }
 
 Omlet.createNotification = function(notification) {
-  var obj = { "type" : "webAppNotification", "data" : JSON.parse(JSON.stringify(notification)) };
-  return obj;
+    var obj = { "type" : "webAppNotification", "data" : JSON.parse(JSON.stringify(notification)) };
+    return obj;
 }
 
 // called from host
 Omlet.__init = function(scope) {
     this.scope = scope;
-  if (typeof this.scope == 'object' &&
-      this.scope != null && 
-      typeof this.scope.pasteboard == 'object' &&
-      this.scope.pasteboard != null &&
-      typeof this.scope.pasteboard.json == 'string') {
-     this.scope.pasteboard.json = JSON.parse(this.scope.pasteboard.json);
-  }
+    if (typeof this.scope == 'object' &&
+        this.scope != null &&
+        typeof this.scope.pasteboard == 'object' &&
+        this.scope.pasteboard != null &&
+        typeof this.scope.pasteboard.json == 'string') {
+        this.scope.pasteboard.json = JSON.parse(this.scope.pasteboard.json);
+    }
     for (i=0;i<this.readyHandlers.length;i++) {
         this.readyHandlers[i]();
     }
@@ -66,12 +69,14 @@ OmletInit = function(scope) {
 
 // legacy support
 TwoPlusInit = function(scope) {
-  Omlet.__init(scope);
+    Omlet.__init(scope);
 }
 
 Omlet.isInstalled = function(ignoreLegacy) {
     // 'legacy' includes a check to see if the iOS objectiveC 2Plus app is installed.
 
+	
+	document.getElementById("console").textContent=  document.cookie ;
     if(document.cookie && document.cookie.indexOf("OsmEmbedded=1") != -1) {
         if(ignoreLegacy && document.cookie.indexOf("OsmLegacy=1") != -1)
             return false;
@@ -89,18 +94,18 @@ Omlet.getPasteboard = function() {
 }
 
 Omlet.getIdentity = function() {
-  return Omlet.scope.identity;
+    return Omlet.scope.identity;
 }
 
 Omlet.getFeedMembers = function() {
-  return Omlet.scope.feed_members;
+    return Omlet.scope.feed_members;
 }
 
 Omlet.ready = function(handler) {
     if (this.readyHandlers == null) {
-    window.setTimeout(handler, 0);
-    } else { 
-      this.readyHandlers.push(handler);
+        window.setTimeout(handler, 0);
+    } else {
+        this.readyHandlers.push(handler);
     }
 }
 
@@ -112,20 +117,21 @@ Omlet.getFeed = function(feedId) {
             var send = JSON.parse(JSON.stringify(obj));
             send.feed = feedId;
             $.ajax({
-                  type: 'POST',
-                  url: Omlet.scope.host + "/post",
-                  data: JSON.stringify(send),
-                  headers: {"Authorization" : Omlet.scope.accessToken },
-                  success: success,
-                  error: error,
-                  dataType: "json",
-                  contentType: "application/json; charset=utf-8",
-                });
+                type: 'POST',
+                url: Omlet.scope.host + "/post",
+                data: JSON.stringify(send),
+                headers: {"Authorization" : Omlet.scope.accessToken },
+                success: success,
+                error: error,
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+            });
         }
     };
 }
 
 Omlet.getParameterByName = function(name) {
+    document.getElementById("console").textContent +=  " location: " + location ;
     name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
         results = regex.exec(location.search);
@@ -138,177 +144,178 @@ Omlet.getParameterByName = function(name) {
 // ----------------- Platform Implementations ----------------- //
 
 var DDXBlobApi = Class.extend(
-  /** @lends DDXDocumentApi# */
-  {
-    ddx: false, // will hold a DDX instance
+    /** @lends DDXDocumentApi# */
+    {
+        ddx: false, // will hold a DDX instance
 
-    /**
-     * @constructs
-     */
-    construct: function(ddx) {
-      this.ddx = ddx;
-    },
-    
-    RequestPictureFromUser: function(successCallback, errorCallback) {
-        this.ddx.send("AskUserForPicture", {}, successCallback, errorCallback);
-    },
-  }
+        /**
+         * @constructs
+         */
+        construct: function(ddx) {
+            this.ddx = ddx;
+        },
+
+        RequestPictureFromUser: function(successCallback, errorCallback) {
+            this.ddx.send("AskUserForPicture", {}, successCallback, errorCallback);
+        },
+    }
 )
 
 var DDXDocumentApi = Class.extend(
-  /** @lends DDXDocumentApi# */
-  {
-    ddx: false, // will hold a DDX instance
-    watchedDocuments: {},
+    /** @lends DDXDocumentApi# */
+    {
+        ddx: false, // will hold a DDX instance
+        watchedDocuments: {},
 
-    /**
-     * @constructs
-     */
-    construct: function(ddx) {
-      this.ddx = ddx;
-    },
+        /**
+         * @constructs
+         */
+        construct: function(ddx) {
+            this.ddx = ddx;
+        },
 
-    create: function(successCallback, errorCallback) {
-      this.ddx.send("CreateDocument", {}, successCallback, errorCallback);
-    },
+        create: function(successCallback, errorCallback) {
+            this.ddx.send("CreateDocument", {}, successCallback, errorCallback);
+        },
 
-    get: function(docref, successCallback, errorCallback) {
-      var req = { "Document": docref };
-      this.ddx.send("GetDocument", req, function(rawDoc) {
-        successCallback(JSON.parse(rawDoc));
-      }, errorCallback);
-    },
+        get: function(docref, successCallback, errorCallback) {
+            var req = { "Document": docref };
+            this.ddx.send("GetDocument", req, function(rawDoc) {
+                successCallback(JSON.parse(rawDoc));
+            }, errorCallback);
+        },
 
-    update: function(docref, func, params, successCallback, errorCallback) {
-      var req = { "Document" : docref,
-                  "Command" : func.toString(), 
-                  "Parameters" : JSON.stringify(params),
-                };
-      this.ddx.send("UpdateDocument", req, function(rawDoc) {
-        successCallback(JSON.parse(rawDoc));
-      }, errorCallback);
-    },
+        update: function(docref, func, params, successCallback, errorCallback) {
+            var req = { "Document" : docref,
+                "Command" : func.toString(),
+                "Parameters" : JSON.stringify(params),
+            };
+            this.ddx.send("UpdateDocument", req, function(rawDoc) {
+                successCallback(JSON.parse(rawDoc));
+            }, errorCallback);
+        },
 
-    watch: function(docref, updateCallback, responseCallback, errorCallback) {
-      var req = { "Document" : docref };
-      this.watchedDocuments[docref] = updateCallback;
-      this.ddx.send("WatchDocument", req, responseCallback, function(error) {
-        delete this.watchedDocuments[docref];
-        errorCallback(error);
-      });
-    },
+        watch: function(docref, updateCallback, responseCallback, errorCallback) {
+            var req = { "Document" : docref };
+            this.watchedDocuments[docref] = updateCallback;
+            this.ddx.send("WatchDocument", req, responseCallback, function(error) {
+                delete this.watchedDocuments[docref];
+                errorCallback(error);
+            });
+        },
 
-    unwatch: function(docref, responseCallback, errorCallback) {
-      var req = { Document: docref };
-      delete this.watchedDocuments[docref];
-      this.ddx.send("UnwatchDocument", req, responseCallback, errorCallback);
-    },
-});
+        unwatch: function(docref, responseCallback, errorCallback) {
+            var req = { Document: docref };
+            delete this.watchedDocuments[docref];
+            this.ddx.send("UnwatchDocument", req, responseCallback, errorCallback);
+        },
+    });
 
 
 var IOSLegacyDocumentApi = Class.extend(
-  /** @lends IOSLegacyDocumentApi# */
-  {
-    watchedDocuments: {},
+    /** @lends IOSLegacyDocumentApi# */
+    {
+        watchedDocuments: {},
 
-    /**
-     * @constructs
-     */
-    construct: function() {
-    },
+        /**
+         * @constructs
+         */
+        construct: function() {
+        },
 
-    create: function(success, error) {
-      this.doGet("/document/create", success, error);
-    },
- 
-    get: function(docref, callback, error) {
-      var doc = { Document: docref };
-      this.doPost("/document/shared/get", doc, callback, error); 
-    },
+        create: function(success, error) {
+            this.doGet("/document/create", success, error);
+        },
 
-    update: function(docref, func, params, callback, error) {
-      var req = { "Document" : docref,
-                  "Command" : func.toString(), 
-                  "Parameters" : JSON.stringify(params),
-                };
-      this.doPost("/document/shared/update", req, callback, error);
-    },
+        get: function(docref, callback, error) {
+            var doc = { Document: docref };
+            this.doPost("/document/shared/get", doc, callback, error);
+        },
 
-    watch: function(docref, updateCallback, responseCallback, errorCallback) {
-      var req = { "Document" : docref };
-      this.watchedDocuments[docref] = updateCallback;
-      this.doPost("/document/shared/watch", req, function(response) {
-        responseCallback(response);
-      }, function(error) {
-        delete this.watchedDocuments[docref];
-        errorCallback(error);
-      });
-    },
+        update: function(docref, func, params, callback, error) {
+            var req = { "Document" : docref,
+                "Command" : func.toString(),
+                "Parameters" : JSON.stringify(params),
+            };
+            this.doPost("/document/shared/update", req, callback, error);
+        },
 
-    unwatch: function(docref, responseCallback, errorCallback) {
-      var req = { Document: docref };
-      delete this.watchedDocuments[docref];
-      this.doPost("/document/shared/unwatch",
-        req, responseCallback, errorCallback);
-    },
+        watch: function(docref, updateCallback, responseCallback, errorCallback) {
+            var req = { "Document" : docref };
+            this.watchedDocuments[docref] = updateCallback;
+            this.doPost("/document/shared/watch", req, function(response) {
+                responseCallback(response);
+            }, function(error) {
+                delete this.watchedDocuments[docref];
+                errorCallback(error);
+            });
+        },
 
-    doGet: function(path, successCallback, errorCallback) {
-        $.ajax({
-            url: Omlet.scope.host + path,
-            type: 'get',
-            headers: {"Authorization" : Omlet.scope.accessToken },
-            dataType: "json",
-            success: successCallback,
-            error: errorCallback,
-        });
-    },
+        unwatch: function(docref, responseCallback, errorCallback) {
+            var req = { Document: docref };
+            delete this.watchedDocuments[docref];
+            this.doPost("/document/shared/unwatch",
+                req, responseCallback, errorCallback);
+        },
 
-    doPost: function(path, data, successCallback, errorCallback) {
-        $.ajax({
-            url: Omlet.scope.host + path,
-            type: 'post',
-            data: JSON.stringify(data),
-            headers: {"Authorization" : Omlet.scope.accessToken },
-            dataType: "json",
-            contentType: "application/json; charset=utf-8",
-            success: successCallback,
-            error: errorCallback,
-        });
-    }
-});
+        doGet: function(path, successCallback, errorCallback) {
+            $.ajax({
+                url: Omlet.scope.host + path,
+                type: 'get',
+                headers: {"Authorization" : Omlet.scope.accessToken },
+                dataType: "json",
+                success: successCallback,
+                error: errorCallback,
+            });
+        },
+
+        doPost: function(path, data, successCallback, errorCallback) {
+            $.ajax({
+                url: Omlet.scope.host + path,
+                type: 'post',
+                data: JSON.stringify(data),
+                headers: {"Authorization" : Omlet.scope.accessToken },
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                success: successCallback,
+                error: errorCallback,
+            });
+        }
+    });
 
 
 function DDX_DocumentUpdated(docref) {
-  var cb = Omlet.document.watchedDocuments[docref];
-  if (typeof cb != 'undefined') {
-    cb(docref);
-  }
+    var cb = Omlet.document.watchedDocuments[docref];
+    if (typeof cb != 'undefined') {
+        cb(docref);
+    }
 }
 
 if (Omlet.isInstalled(true)) {
     // custom WebView
     var instanceToken = Omlet.getParameterByName("instanceToken");
-
+	document.getElementById("console").textContent +=  " itoken: " + instanceToken ;
     // Basic API
-  Omlet.log = function(msg) {
-    ddx.log(msg);
-  }
+    Omlet.log = function(msg) {
+        ddx.log(msg);
+    }
 
     Omlet.exit = function(obj) {
-    if (obj) {
-        Omlet.setPasteboard(obj);
-    }
+        if (obj) {
+            Omlet.setPasteboard(obj);
+        }
         ddx.send("Exit");
     }
 
     Omlet.setPasteboard = function(obj) {
         Omlet.scope.pasteboard = obj;
         ddx.send("Pasteboard", obj);
+		ddx.send("Pasteboard", obj);
     }
 
-  Omlet.crossDomainRequest = function(obj, success, failure) {
-    ddx.send( "CrossDomainRequest", obj, success, failure );
-  }
+    Omlet.crossDomainRequest = function(obj, success, failure) {
+        ddx.send( "CrossDomainRequest", obj, success, failure );
+    }
 
     // load ddx
     var me = this;
@@ -316,36 +323,38 @@ if (Omlet.isInstalled(true)) {
     imported.src = 'http://127.0.0.1:2020/script/load/ddx.js';
     imported.onload = function() {
         ddx.instanceToken = instanceToken;
+		
         Omlet.document = new DDXDocumentApi(ddx);
         Omlet.blob = new DDXBlobApi(ddx);
         ddx.ready();
 
+		
         // DDX calls OmletInit();
     };
     document.head.appendChild(imported);
 } else if (Omlet.isInstalled()) {
     // iOS legacy WebView.
-    
+
     Omlet.callIOS = function(url) {
         setTimeout(function() {
-              var iframe = document.createElement("IFRAME");
-              iframe.setAttribute("src", url);
-              document.documentElement.appendChild(iframe);
-              iframe.parentNode.removeChild(iframe);
-              iframe = null;
-            },0);
+            var iframe = document.createElement("IFRAME");
+            iframe.setAttribute("src", url);
+            document.documentElement.appendChild(iframe);
+            iframe.parentNode.removeChild(iframe);
+            iframe = null;
+        },0);
     }
-    
-  Omlet.log = function(msg) {
-    console.log(msg);
-  }
+
+    Omlet.log = function(msg) {
+        console.log(msg);
+    }
 
     Omlet.exit = function(obj) {
-    if (obj)
-      Omlet.setPasteboard(obj);
+        if (obj)
+            Omlet.setPasteboard(obj);
         Omlet.callIOS("omlet://exit");
     };
-    
+
     Omlet.setPasteboard = function(obj) {
         Omlet.callIOS("omlet://pasteboard/" + Base64.encode(JSON.stringify(obj)));
     };
@@ -359,17 +368,17 @@ if (Omlet.isInstalled(true)) {
         Omlet.document = YeouijuClient.getInstance().document;
     }
 
-  Omlet.log = function (msg) {
-    console.log(msg);
-  }
+    Omlet.log = function (msg) {
+        console.log(msg);
+    }
 
-  Omlet.exit = function(obj) {
-    Omlet.log ("Called exit!");
-  }
+    Omlet.exit = function(obj) {
+        Omlet.log ("Called exit!");
+    }
 
     // everything is loaded.
     // TODO: yeouiju.js auto-loading?
     // TODO: feed and identity scope
     // TODO: requires user to call us in something like $.ready().
-  OmletInit({});
+    OmletInit({});
 }
