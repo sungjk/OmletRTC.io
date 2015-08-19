86f5af6e302bf7f21c8b4ac9fba4efcb4073f56c
sungjungkim <ajax0615@gmail.com>
2015-08-18 18:55:48 -0700

    update

--- src/webrtcjs/webrtc.io.js
+++ src/webrtcjs/webrtc.io.js
@@ -434,6 +434,51 @@
     rtc.sendOffers();
   });
 
+
+
+  function log(message){
+    var logArea = document.getElementById("console");
+    logArea.value += message + '\n';
+    logArea.scrollTop = logArea.scrollHeight;
+  }
+
+  // Fallbacks for vendor-specific variables until the spec is finalized.
+
+  var PeerConnection = (window.PeerConnection || window.webkitPeerConnection00 || window.webkitRTCPeerConnection || window.mozRTCPeerConnection);
+  var URL = (window.URL || window.webkitURL || window.msURL || window.oURL);
+  var getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+  var nativeRTCIceCandidate = (window.mozRTCIceCandidate || window.RTCIceCandidate);
+  var nativeRTCSessionDescription = (window.mozRTCSessionDescription || window.RTCSessionDescription); // order is very important: "RTCSessionDescription" defined in Nighly but useless
+
+  var sdpConstraints = {
+    'mandatory': {
+      'OfferToReceiveAudio': true,
+      'OfferToReceiveVideo': true
+    }
+  };
+
+  if (navigator.webkitGetUserMedia) {
+    if (!webkitMediaStream.prototype.getVideoTracks) {
+      webkitMediaStream.prototype.getVideoTracks = function() {
+        return this.videoTracks;
+      };
+      webkitMediaStream.prototype.getAudioTracks = function() {
+        return this.audioTracks;
+      };
+    }
+
+    // New syntax of getXXXStreams method in M26.
+    if (!webkitRTCPeerConnection.prototype.getLocalStreams) {
+      webkitRTCPeerConnection.prototype.getLocalStreams = function() {
+        return this.localStreams;
+      };
+      webkitRTCPeerConnection.prototype.getRemoteStreams = function() {
+        return this.remoteStreams;
+      };
+    }
+  }
+
+
 }).call(this);
 
 
@@ -509,49 +554,4 @@ function mergeConstraints(cons1, cons2) {
   }
   merged.optional.concat(cons2.optional);
   return merged;
-}
-
-
-
-
-function log(message){
-  var logArea = document.getElementById("console");
-  logArea.value += message + '\n';
-  logArea.scrollTop = logArea.scrollHeight;
-}
-
-// Fallbacks for vendor-specific variables until the spec is finalized.
-
-var PeerConnection = (window.PeerConnection || window.webkitPeerConnection00 || window.webkitRTCPeerConnection || window.mozRTCPeerConnection);
-var URL = (window.URL || window.webkitURL || window.msURL || window.oURL);
-var getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-var nativeRTCIceCandidate = (window.mozRTCIceCandidate || window.RTCIceCandidate);
-var nativeRTCSessionDescription = (window.mozRTCSessionDescription || window.RTCSessionDescription); // order is very important: "RTCSessionDescription" defined in Nighly but useless
-
-var sdpConstraints = {
-  'mandatory': {
-    'OfferToReceiveAudio': true,
-    'OfferToReceiveVideo': true
-  }
-};
-
-if (navigator.webkitGetUserMedia) {
-  if (!webkitMediaStream.prototype.getVideoTracks) {
-    webkitMediaStream.prototype.getVideoTracks = function() {
-      return this.videoTracks;
-    };
-    webkitMediaStream.prototype.getAudioTracks = function() {
-      return this.audioTracks;
-    };
-  }
-
-  // New syntax of getXXXStreams method in M26.
-  if (!webkitRTCPeerConnection.prototype.getLocalStreams) {
-    webkitRTCPeerConnection.prototype.getLocalStreams = function() {
-      return this.localStreams;
-    };
-    webkitRTCPeerConnection.prototype.getRemoteStreams = function() {
-      return this.remoteStreams;
-    };
-  }
 }
