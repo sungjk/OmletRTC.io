e490da09ded0f81aff4836fbaecad4e418b203fd
sungjungkim <ajax0615@gmail.com>
2015-08-18 17:17:16 -0700

    User defined code

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,9 @@
-// Application Code
-function get(id){
-  return document.getElementById(id);
-}
+//////////////////////////////////////////////////////////////////
+//
+//                Omlet User Definded code
+//
+/////////////////////////////////////////////////////////////////
 
-function getQuery(id) {
-  return document.querySelector(id);
-}
-
-var localVideo = getQuery("#localVideo");
-var remoteVideo = getQuery("#remoteVideo");
-
-
-/*
-* Omlet document information - User defined
-*/
 omletrtc.doc_info = {
   callback: "http://203.246.112.144:3310/index.html",
   appName: "OmletRTC",
@@ -23,10 +13,161 @@ omletrtc.doc_info = {
   displayText: 'Real Time Video Chat!\nClick here to start!',
 };
 
+omletrtc.initConnection = function () {
+  var chatId = 100;
+  var identity = null;
+  var numOfUser = 0;
+
+  // Connection info
+  var info = {
+    'chatId' : chatId,
+    'creator' : null,
+    'sender' : null,
+    'numOfUser' : numOfUser,
+    'userJoin' : false,
+    'sessionDescription' : null,
+    'candidate' : null,
+    'id' : null,
+    'label' : null,
+    'timestamp' : Date.now()
+  };
+
+  return info;
+};
+
+/*
+ * Function for parameter handling documentApi.update:
+ * function(reference, func, parameters, success, error)
+ */
+omletrtc.addMessage = function (old, parameters) {
+  if (parameters.userJoin) {
+    old.sender = parameters.sender;
+    old.userJoin = true;
+  } else {
+    old.sender = parameters.sender;
+    old.userJoin = false;
+  }
+
+  if (parameters.sessionDescription) {
+    old.sender = parameters.sender;
+    old.sessionDescription = parameters.sessionDescription;
+  }
+  if (parameters.candidate) {
+    old.sender = parameters.sender;
+    old.candidate = parameters.candidate;
+    old.id = parameters.id;
+    old.label = parameters.label;
+  }
+  if (old.numOfUser == 0){
+    old.numOfUser = old.numOfUser+1;
+  }
+  if (parameters.creator){
+    old.creator = parameters.creator;
+  }
+  old.timestamp = Date.now();
+
+  return old;
+};
+
+omletrtc.handleMessage = function (doc) {
+  omletrtc.chatDoc = doc;
+  var chatDoc = omletrtc.chatDoc;
+
+  if (chatDoc.numOfUser > 2)
+    return ;
+
+  if (chatDoc.userJoin && chatDoc.creator.name === Omlet.getIdentity().name) {
+    log('[+] sender: ' + chatDoc.sender + ', message: userJoin');
+
+    var param_userJoin = {
+      userJoin : false,
+      sender : Omlet.getIdentity().name
+    };
+    omletrtc.documentApi.update(omletrtc.myDocId, omletrtc.addMessage, param_userJoin, function () {
+      omletrtc.documentApi.get(omletrtc.myDocId, function () {});
+      omletrtc.createOffer();
+    }, function (error) {
+      log("[-] upate-userJoin: " + error);
+    });
+  }
+
+  if (chatDoc.sessionDescription && omletrtc.flag) {
+    if (chatDoc.sessionDescription.type === 'answer' && chatDoc.creator.name === Omlet.getIdentity().name) {
+      omletrtc.peerConnection.setRemoteDescription(new RTCSessionDescription(chatDoc.sessionDescription), function () {
+        log('[+] setRemoteSDP_Answer.');
+      }, function (error) {
+        log('[-] setRemoteSDP_Answer: ' + error);
+      });
+
+      omletrtc.flag = false;
+    } else if (chatDoc.sessionDescription.type === 'offer' && chatDoc.creator.name !== Omlet.getIdentity().name) {
+      omletrtc.peerConnection.setRemoteDescription(new RTCSessionDescription(chatDoc.sessionDescription), function () {
+        log('[+] setRemoteSDP_Offer.');
+        if (omletrtc.peerConnection.remoteDescription.type === 'offer') {
+          omletrtc.createAnswer();
+        }
+      }, function (error) {
+        log('[-] setRemoteSDP_Offer: ' + error);
+      });
+
+      omletrtc.flag = false;
+    }
+  }
+
+  if (chatDoc.candidate && chatDoc.sender !== Omlet.getIdentity().name) {
+    log('[+] sender: ' + chatDoc.sender + ', candidate: ' + chatDoc.candidate);
+
+    var candidate = new RTCIceCandidate({
+      candidate : chatDoc.candidate,
+      sdpMLineIndex : chatDoc.label
+    }, function () {
+      log('[+] Success to AddIceCandidate.');
+    }, function (error) {
+      log('[-] handleMessage-RTCIceCandidate: ' + error);
+    });
+
+    log('[+] addIceCandidate.');
+    omletrtc.peerConnection.addIceCandidate(candidate);
+  }
+};
+
+
+Omlet.ready(function() {
+  log("[+] Omlet is Ready.");
+  // log("UA", navigator.userAgent.toString());
+
+  if (omletrtc.hasDocument()) {
+    log("[+] Initializing DocumentAPI.");
+    omletrtc.initDocumentAPI();
+  } else {
+    log("[-] Doc is not found.");
+    omletrtc.initDocumentAPI();
+  }
+});
+
+
+
+// Application Code
+
+function get(id){
+  return document.getElementById(id);
+}
+
+function getQuery(id) {
+  return document.querySelector(id);
+}
+
+function log(message){
+  var logArea = get("console");
+  logArea.value += message + '\n';
+  logArea.scrollTop = logArea.scrollHeight;
+}
+
+
+var localVideo = getQuery("#localVideo");
+var remoteVideo = getQuery("#remoteVideo");
 
 function joinAV() {
-  log("[+] joinAV");
-
   var documentApi = omletrtc.documentApi;
 
   documentApi.update(omletrtc.myDocId, omletrtc.addMessage, {}, function () {
@@ -37,8 +178,6 @@ function joinAV() {
 
   // Caller
   if (omletrtc.chatDoc.numOfUser == 0){
-  //if (chatDoc.creator.name === Omlet.getIdentity().name) {
-    log("[+] " + Omlet.getIdentity().name + " creates the room.");
     var identity = Omlet.getIdentity();
     var param_creator = {
       creator : identity
@@ -49,7 +188,6 @@ function joinAV() {
       log("[-] update-creator: " + error);
     });
     documentApi.get(omletrtc.myDocId, function (doc) {
-      log("creator22: "+ doc.creator.name);
       omletrtc.chatDoc = doc;
       navigator.getUserMedia(omletrtc.constraints, omletrtc.handleUserMedia, function (error) {
         log("[-] joinAV-getUserMedia-caller: " + error);
@@ -62,16 +200,3 @@ function joinAV() {
     });
   }
 }
-
-Omlet.ready(function() {
-  log("[+] Omlet is Ready.");
-  log("UA", navigator.userAgent.toString());
-
-  if (omletrtc.hasDocument()) {
-    log("[+] Initializing DocumentAPI.");
-    omletrtc.initDocumentAPI();
-  } else {
-    log("[-] Doc is not found.");
-    omletrtc.initDocumentAPI();
-  }
-});
